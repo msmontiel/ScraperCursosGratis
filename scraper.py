2413b11737076b57d0c0b51f03772533f508754f
Matías Montiel <matias.montiel@usach.cl>
2021-03-28 18:28:16 -0300

    Se modifica el archivo .py para que agregue todos los links a un archivo .txt en la carpeta correspondiente al día

--- scraper.py
+++ scraper.py
@@ -6,56 +6,43 @@ import datetime
 HOME_URL = 'https://freebiesglobal.com/dealstore/udemy'
 HOME_URL2 = 'https://freebiesglobal.com/dealstore/udemy/page/2'
 
-XPATH_LINK_TO_ARTHICLE ='//a[@class="img-centered-flex rh-flex-center-align rh-flex-justify-center"]/@href'
-XPATH_TITLE = '//div[@class="title_single_area"]/text-fill[not(@class)]/text()'
+XPATH_LINK_TO_ARTHICLE = '//a[@class="img-centered-flex rh-flex-center-align rh-flex-justify-center"]/@href'
 XPATH_UDEMY_LINK = '//div/a[@class="re_track_btn btn_offer_block"]/@href'
 
 
-
-def parse_notice(link, today,i):
+def parse_notice(link, today):
     try:
         response = requests.get(link)
-        
+
         if response.status_code == 200:
             notice = response.content.decode('utf-8')
             parsed = html.fromstring(notice)
             link_udemy = parsed.xpath(XPATH_UDEMY_LINK)[0]
-            print(link_udemy)
-            try:
-                title = parsed.xpath(XPATH_TITLE)[0]
-                print(title)
-                
-                
-            except IndexError:
-                return
-
-            with open(f'{today}/file{i}.txt', 'w', encoding='utf-8') as f:
+            with open(f'{today}/links.txt', 'a') as f:
                 f.write(link_udemy)
-               
+                f.write("\n")
+                f.close()
+
         else:
             raise ValueError(f'Error: {response.status_code}')
     except ValueError as ve:
         print(ve)
 
 
-def parse_home():
+def parse_home(url_in):
     try:
-        response = requests.get(HOME_URL2)
+        response = requests.get(url_in)
         if response.status_code == 200:
             home = response.content.decode('utf-8')
             parsed = html.fromstring(home)
             links_to_notice = parsed.xpath(XPATH_LINK_TO_ARTHICLE)
-            #print(links_to_notice)
-            i=0
 
             today = datetime.date.today().strftime('%d-%m-%Y')
             if not os.path.isdir(today):
                 os.mkdir(today)
 
-
             for link in links_to_notice:
-                i+=1
-                parse_notice(link, today, i)
+                parse_notice(link, today)
         else:
             raise ValueError(f'Error: {response.status_code}')
 
@@ -63,9 +50,9 @@ def parse_home():
         print(ve)
 
 
-
 def run():
-    parse_home()
+    parse_home(HOME_URL)
+    parse_home(HOME_URL2)
 
 
 if __name__ == '__main__':
